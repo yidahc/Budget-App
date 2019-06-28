d68c7e7f5d970ffa74f6cfc6d2d98d98c81158c2
yidahc <yidah.a.curiel@gmail.com>
2019-06-28 18:11:39 -0500

    trying to deploy with heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
-const app = require('./config.js');
-const port = process.env.PORT || 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const port = process.env.PORT || 3000;
+
+// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
+// var items = require('../database-mysql');
+// var items = require('../database-mongo');
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+// UNCOMMENT FOR ANGULAR
+// app.use(express.static(__dirname + '/../angular-client'));
+// app.use(express.static(__dirname + '/../node_modules'));
+
+app.get('/today', routes.getDaily);
+
+app.get('/totals', routes.getTotals);
+
+app.post('/daily', routes.addToDaily);
+
+app.get('/daily', routes.getAllDaily);
 
 app.listen(port, () => {
   console.log('listening on port 3000!');
 });
+
+module.exports = app;
