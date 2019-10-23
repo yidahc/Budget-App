188f7c81d8461082f20e75a0c61c2b7133513a6e
yidahc <yidah.a.curiel@gmail.com>
2019-10-22 19:58:58 -0500

    adding personal reference notes to backend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+// NodeRequire importing express module.
+
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const port = process.env.PORT || 3000;
 
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
 
+// Creates an express application
 const app = express();
 app.use(
   bodyParser.urlencoded({
@@ -14,16 +14,23 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+// static middleware method serves all files by adding to the end of this provided root directory url. 
+// serving up the html (which in turn serves react components)
 app.use(express.static(__dirname + '/../react-client/dist'));
+// Instead of sending a 404 response, this module will call next() to move on to the next middleware
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
+// Creating server endpoint called today, using the express 'get' method, which takes 2 parameters (string, cb) (pathName, requestHandler)
+// '/today' is the path name appended to the root directory when serving this path
+// getDaily method is the route the path will take (handles what happes when we make a request from the frontend)
 app.get('/today', routes.getDaily);
 
+// we are listening from the server for any get requests made to the '/totals' endpoint/path, 
+// and executing the getTotals method when we receive a request at this endpoint
 app.get('/totals', routes.getTotals);
 
+// 'post' method receives the request (what you are trying to submit to the database) 
+// and passes it as a parameter to execute the cb (which dissects the request and sumbits it to the db)
 app.post('/daily', routes.addToDaily);
 
 app.get('/daily', routes.getAllDaily);
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 const db = require('../database-mysql/index.js');
-
+// importing db methods
 /*
 module.exports.getCat = (request, response) => {
   db.getCat( data => {
@@ -11,7 +11,9 @@ module.exports.getCat = (request, response) => {
 };
 */
 
+// exporting getDaily method to be used as a route for a specific endpoint
 module.exports.getDaily = (request, response) => {
+  // invoking getToday function imported from database
   db.getToday( data => {
   response
     .status(200)
@@ -20,7 +22,10 @@ module.exports.getDaily = (request, response) => {
   });
 };
 
+// routes are methods with callbacks in them. Once we receive the data, we use it in callbacks
+// getTotals 
 module.exports.getTotals = (request, response) => {
+  // db.getTotals method returns results from database (data), which is then used in the callback below 
   db.getTotals( data => {
   response
     .status(200)
